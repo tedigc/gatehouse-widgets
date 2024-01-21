323b609abfd50d51ef77035efd8183bd60e0cd5a
tedigc <tedigc@gmail.com>
2024-01-21 23:29:00 +0000

    feat: add important

--- src/Form.tsx
+++ src/Form.tsx
@@ -11,11 +11,11 @@ export type FormProps = {
 
 export const Form = ({ config }: FormProps) => {
   return (
-    <form className="flex w-full flex-col items-stretch rounded-md border border-gray-300 bg-gray-100 p-4">
-      <div className="mb-4">
+    <form className="!flex !w-full !flex-col !items-stretch !rounded-md !border !border-gray-300 !bg-gray-100 !p-4">
+      <div className="!mb-4">
         <label
           htmlFor="email"
-          className="mb-2 block text-sm font-medium leading-6 text-gray-900"
+          className="!mb-2 !block !text-sm !font-medium !leading-6 !text-gray-900"
         >
           Email address
         </label>
@@ -25,13 +25,13 @@ export const Form = ({ config }: FormProps) => {
           type="email"
           autoComplete="email"
           required
-          className="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          className="!block !w-full !rounded-md !border-0 !px-3 !py-1.5 !text-gray-900 !shadow-sm !ring-1 !ring-inset !ring-gray-300 !placeholder:text-gray-400 !focus:ring-2 !focus:ring-inset !focus:ring-indigo-600 !sm:text-sm sm:leading-6"
         />
       </div>
 
       <button
         type="button"
-        className="flex min-h-[36px] w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+        className="!flex !min-h-[36px] !w-full !justify-center !rounded-md !px-3 !py-1.5 !text-sm !font-semibold !leading-6 !shadow-sm !focus-visible:outline !focus-visible:outline-2 !focus-visible:outline-offset-2"
         style={{
           backgroundColor: config.buttonBackgroundColor,
           color: config.buttonForegroundColor,
