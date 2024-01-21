6e70d53e6306c8705d1a904a9993de385e78cf81
tedigc <tedigc@gmail.com>
2024-01-21 18:46:54 +0000

    feat: add dist

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { Form, FormConfig } from "./Form";
 
-function App() {
-  const [count, setCount] = useState(0)
+const defaultConfig = {
+  buttonBackgroundColor: "#77DB89",
+  buttonForegroundColor: "#000",
+  submitButtonText: "Submit",
+  fileUrl: `https://media.wizards.com/2014/downloads/dnd/DMBasicRulesv.0.3_PrinterFriendly.pdf`,
+};
+
+const App = () => {
+  const [config, setConfig] = useState<FormConfig | null>(defaultConfig);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  const fetchConfig = async () => {
+    try {
+      // const response = fetch("");
+      // const json = (await response).json() as unknown as FormConfig;
+      // setConfig(json);
+
+      setConfig(defaultConfig);
+    } catch (e) {
+      setError("Something went wrong!");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchConfig();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {isLoading && <p>Loading...</p>}
+      {config && <Form config={config} />}
+      {error && <p>{error}</p>}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
