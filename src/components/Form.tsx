0ed93020552d6a2f5d8632eb8139da2311aab9e0
tedigc <tedigc@gmail.com>
2024-01-24 17:14:14 +0000

    feat: turn widget into modal

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,5 @@
-import { useRef } from "react";
+import { Dialog } from "@headlessui/react";
+import { useState } from "react";
 
 export type FormConfig = {
   fileUrl: string;
@@ -6,7 +7,9 @@ export type FormConfig = {
   textColour: string;
   buttonColour: string;
   buttonTextColour: string;
-  submitButtonText: string;
+  formHeading: string;
+  formText: string;
+  formButtonText: string;
 };
 
 export type FormProps = {
@@ -14,15 +17,10 @@ export type FormProps = {
 };
 
 export const Form = ({ config }: FormProps) => {
-  const modalRef = useRef<HTMLDialogElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
-    const modal = modalRef.current;
-    if (modal?.open) {
-      modal?.close();
-    } else {
-      modal?.showModal();
-    }
+    setIsOpen(true);
   };
 
   return (
@@ -30,7 +28,7 @@ export const Form = ({ config }: FormProps) => {
       <button
         type="button"
         onClick={handleClick}
-        className="!px-3 !py-2.5 !rounded-lg"
+        className="!px-3 !py-1.5 !text-sm !font-semibold !leading-6 !rounded-lg"
         style={{
           backgroundColor: config.buttonColour,
           color: config.buttonTextColour,
@@ -38,43 +36,54 @@ export const Form = ({ config }: FormProps) => {
       >
         Get it now
       </button>
-      <dialog ref={modalRef}>
-        <form
-          className="!flex !w-full !flex-col !items-stretch !rounded-md !border !border-gray-300 !p-4"
-          style={{
-            backgroundColor: config.backgroundColour,
-            color: config.textColour,
-          }}
-        >
-          <div className="!mb-4">
-            <label
-              htmlFor="email"
-              className="!mb-2 !block !text-sm !font-medium !leading-6 !text-gray-900"
-            >
-              Email address
-            </label>
-            <input
-              id="email"
-              name="email"
-              type="email"
-              autoComplete="email"
-              required
-              className="!block !w-full !rounded-md !border-0 !px-3 !py-1.5 !text-gray-900 !shadow-sm !ring-1 !ring-inset !ring-gray-300 !placeholder:text-gray-400 !focus:ring-2 !focus:ring-inset !focus:ring-indigo-600 !sm:text-sm sm:leading-6"
-            />
-          </div>
-
-          <button
-            type="button"
-            className="!flex !min-h-[36px] !w-full !justify-center !rounded-md !px-3 !py-1.5 !text-sm !font-semibold !leading-6 !shadow-sm !focus-visible:outline !focus-visible:outline-2 !focus-visible:outline-offset-2"
+      <Dialog
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        className="relative z-10"
+      >
+        <div className="!w-screen !h-screen !fixed !top-0 !left-0 !right-0 !bottom-0 !bg-zinc-800 !opacity-80 !-z-10" />
+        <Dialog.Panel className="!w-full !max-w-[486px] !mx-auto !my-auto !z-10">
+          <form
+            className="!flex !w-full !flex-col !items-stretch !rounded-lg !border !border-gray-300 !p-8"
             style={{
-              backgroundColor: config.buttonColour,
-              color: config.buttonTextColour,
+              backgroundColor: config.backgroundColour,
+              color: config.textColour,
             }}
           >
-            {config.submitButtonText}
-          </button>
-        </form>
-      </dialog>
+            <h1 className="!text-xl !font-extrabold !mb-2">
+              {config.formHeading}
+            </h1>
+            <p className="!mb-8">{config.formText}</p>
+            <div className="!mb-4">
+              <label
+                htmlFor="email"
+                className="!mb-2 !block !text-sm !font-regular !leading-[1]"
+              >
+                Email address
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="!block !w-full !rounded-lg !border-0 !text-md !px-3 !py-2 !shadow-sm !ring-1 !ring-inset !ring-gray-300 !placeholder:text-gray-400 !focus:ring-2 !focus:ring-inset"
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="!flex !min-h-[36px] !w-full !justify-center !rounded-lg !px-3 !py-2 !text-sm !font-semibold !leading-6 !focus-visible:outline !focus-visible:outline-2 !focus-visible:outline-offset-2"
+              style={{
+                backgroundColor: config.buttonColour,
+                color: config.buttonTextColour,
+              }}
+            >
+              {config.formButtonText}
+            </button>
+          </form>
+        </Dialog.Panel>
+      </Dialog>
     </>
   );
 };
