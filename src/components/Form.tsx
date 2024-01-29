4f49a37614bfd2805c5c6ad77f6bc87ad6a29e70
tedigc <tedigc@gmail.com>
2024-01-29 20:29:43 +0000

    feat: add validation to form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,5 @@
 import { Dialog } from "@headlessui/react";
-import { useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 export type FormConfig = {
   fileUrl: string;
@@ -18,9 +18,20 @@ export type FormProps = {
 
 export const Form = ({ config }: FormProps) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [email, setEmail] = useState("");
 
   const handleClick = () => {
     setIsOpen(true);
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.currentTarget.value;
+    setEmail(value);
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(email);
   };
 
   return (
@@ -44,6 +55,7 @@ export const Form = ({ config }: FormProps) => {
         <div className="!w-screen !h-screen !fixed !top-0 !left-0 !right-0 !bottom-0 !bg-zinc-800 !opacity-80 !-z-10" />
         <Dialog.Panel className="!w-full !max-w-[486px] !mx-auto !my-auto !z-10">
           <form
+            onSubmit={handleSubmit}
             className="!flex !w-full !flex-col !items-stretch !rounded-lg !border !border-gray-300 !p-8"
             style={{
               backgroundColor: config.backgroundColour,
@@ -68,6 +80,8 @@ export const Form = ({ config }: FormProps) => {
                 autoComplete="email"
                 required
                 className="!block !w-full !rounded-lg !border-0 !text-md !px-3 !py-2 !shadow-sm !ring-1 !ring-inset !ring-gray-300 !placeholder:text-gray-400 !focus:ring-2 !focus:ring-inset"
+                value={email}
+                onChange={handleChange}
               />
             </div>
 
