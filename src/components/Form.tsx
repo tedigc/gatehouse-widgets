64a70d77ea56c58c0181c3b2c7e6650e6ff643fe
tedigc <tedigc@gmail.com>
2024-05-25 22:01:59 +0100

    feat: fix positioning of modal

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -76,7 +76,7 @@ export const Form = ({ gateId, config }: FormProps) => {
       <button
         type="button"
         onClick={handleOpen}
-        className="!px-3 !py-1.5 !text-sm !font-semibold !leading-6 !rounded-lg text-white"
+        className="px-3 py-1.5 text-sm font-semibold leading-6 rounded-lg text-white"
         style={{
           backgroundColor: config.buttonColor,
           color: config.buttonTextColor,
@@ -84,27 +84,31 @@ export const Form = ({ gateId, config }: FormProps) => {
       >
         Get it now
       </button>
-      <Dialog open={isOpen} onClose={handleClose} className="relative z-10">
-        <div className="!w-screen !h-screen !fixed !top-0 !left-0 !right-0 !bottom-0 !bg-zinc-800 !opacity-80 !-z-10" />
-        <Dialog.Panel className="!w-full !max-w-[486px] !mx-auto !my-auto !z-10">
+      <Dialog
+        open={isOpen}
+        onClose={handleClose}
+        className="fixed top-0 left-0 right-0 z-10"
+      >
+        <div className="w-screen h-screen fixed top-0 left-0 right-0 bottom-0 bg-zinc-800 opacity-80 -z-10" />
+        <Dialog.Panel className="w-full max-w-[486px] mx-auto my-auto z-10 mt-20">
           <form
             onSubmit={handleSubmit}
-            className="!flex !w-full !flex-col !items-stretch !rounded-lg !border !border-gray-300 !p-8 bg-white"
+            className="flex w-full flex-col items-stretch rounded-lg border border-gray-300 p-8 bg-white"
             style={{
               backgroundColor: config.backgroundColor,
               color: config.textColor,
             }}
           >
-            <h1 className="!text-xl !font-extrabold !mb-2 text-black tracking-wide">
+            <h1 className="text-xl font-extrabold mb-2 text-black tracking-wide">
               {config.title}
             </h1>
-            <p className="!mb-8 text-black">{config.subheader}</p>
+            <p className="mb-8 text-black">{config.subheader}</p>
 
             <div className="flex justify-between mb-4 gap-4">
               <div>
                 <label
                   htmlFor="firstName"
-                  className="!mb-2 !block !text-sm !font-regular !leading-[1]"
+                  className="mb-2 block text-sm font-regular leading-[1]"
                 >
                   First name
                 </label>
@@ -114,7 +118,7 @@ export const Form = ({ gateId, config }: FormProps) => {
                   type="firstName"
                   autoComplete="firstName"
                   required
-                  className="!block !w-full !rounded-lg !border-0 !text-sm !px-3 !py-2 !shadow-sm !ring-1 !ring-inset !ring-gray-300 !placeholder:text-gray-400 !focus:ring-2 !focus:ring-inset"
+                  className="block w-full rounded-lg border-0 text-sm px-3 py-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset"
                   value={firstName}
                   onChange={handleChangeFirstName}
                 />
@@ -122,7 +126,7 @@ export const Form = ({ gateId, config }: FormProps) => {
               <div>
                 <label
                   htmlFor="email"
-                  className="!mb-2 !block !text-sm !font-regular !leading-[1]"
+                  className="mb-2 block text-sm font-regular leading-[1]"
                 >
                   Last name
                 </label>
@@ -132,17 +136,17 @@ export const Form = ({ gateId, config }: FormProps) => {
                   type="lastName"
                   autoComplete="lastName"
                   required
-                  className="!block !w-full !rounded-lg !border-0 !text-sm !px-3 !py-2 !shadow-sm !ring-1 !ring-inset !ring-gray-300 !placeholder:text-gray-400 !focus:ring-2 !focus:ring-inset"
+                  className="block w-full rounded-lg border-0 text-sm px-3 py-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset"
                   value={lastName}
                   onChange={handleChangeLastName}
                 />
               </div>
             </div>
 
-            <div className="!mb-4">
+            <div className="mb-4">
               <label
                 htmlFor="email"
-                className="!mb-2 !block !text-sm !font-regular !leading-[1]"
+                className="mb-2 block text-sm font-regular leading-[1]"
               >
                 Email address
               </label>
@@ -152,7 +156,7 @@ export const Form = ({ gateId, config }: FormProps) => {
                 type="email"
                 autoComplete="email"
                 required
-                className="!block !w-full !rounded-lg !border-0 !text-sm !px-3 !py-2 !shadow-sm !ring-1 !ring-inset !ring-gray-300 !placeholder:text-gray-400 !focus:ring-2 !focus:ring-inset"
+                className="block w-full rounded-lg border-0 text-sm px-3 py-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset"
                 value={email}
                 onChange={handleChangeEmail}
               />
@@ -160,7 +164,7 @@ export const Form = ({ gateId, config }: FormProps) => {
 
             <button
               type="submit"
-              className="!flex !min-h-[36px] !w-full !justify-center !rounded-lg !px-3 !py-2 !text-sm !font-semibold !leading-6 !focus-visible:outline !focus-visible:outline-2 !focus-visible:outline-offset-2 text-white mb-8"
+              className="flex min-h-[36px] w-full justify-center rounded-lg px-3 py-2 text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 text-white mb-8"
               style={{
                 backgroundColor: config.buttonColor,
                 color: config.buttonTextColor,
